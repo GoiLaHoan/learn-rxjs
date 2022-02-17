75659daff0e24b1c9814efbf2e4c2a1c7395baea
NVH <nvhoan911@gmail.com>
2022-02-17 14:14:35 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from "react";
+import "./App.css";
+import { of, interval, concat, Subject, noop } from "rxjs";
+import {
+    takeWhile,
+    takeUntil,
+    scan,
+    startWith,
+    repeatWhen,
+    share,
+    filter,
+    tap,
+} from "rxjs/operators";
+const countdown$ = interval(250).pipe(
+    startWith(5),
+    scan((time) => time - 1),
+    takeWhile((time) => time > 0),
+    share()
+);
 
+const actions$ = new Subject();
+const snooze$ = actions$
+    .pipe(filter((action) => action === "snooze"))
+    .pipe(tap((x) => console.log(x)));
+const dismiss$ = actions$.pipe(filter((action) => action === "dismiss"));
+const snoozableAlarm$ = concat(countdown$, of("Được rồi đi thôi!")).pipe(
+    repeatWhen(() => snooze$)
+);
+
+const observable$ = concat(
+    snoozableAlarm$.pipe(takeUntil(dismiss$)),
+    of("Đã dậy")
+);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState();
+
+    useEffect(() => {
+        const sub = observable$.subscribe(setState, noop, () =>
+            console.log("complete")
+        );
+        return () => sub.unsubscribe();
+    }, []);
+    return (
+        <>
+            <h3>Alarm Clock</h3>
+            <div className="display">{state}</div>
+            <button className="snooze" onClick={() => actions$.next("snooze")}>
+                Snooze
+            </button>
+            <button
+                className="dismiss"
+                onClick={() => actions$.next("dismiss")}
+            >
+                Dismiss
+            </button>
+        </>
+    );
 }
 
 export default App;
